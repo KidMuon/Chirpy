607b13d3be52013fb93591f2dbc74519fcf1fa03
Dominique Berry <dominique174466@gmail.com>
2024-10-07 20:17:28 -0400

    Framework for adding in and working with the database established.

--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"sync/atomic"
+
+	"github.com/KidMuon/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverhits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+	dbQueries := database.New(db)
+
 	var cfg apiConfig
+	cfg.db = dbQueries
 
 	mux := http.NewServeMux()
 	appHandler := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(appHandler)))
-	mux.HandleFunc("/healthz/", handleHealthz)
-	mux.HandleFunc("/metrics/", cfg.handleServeMetric)
-	mux.HandleFunc("/reset/", cfg.handleResetMetric)
+	mux.HandleFunc("GET /api/healthz", handleHealthz)
+	mux.HandleFunc("GET /admin/metrics", cfg.handleServeMetric)
+	mux.HandleFunc("POST /admin/reset", cfg.handleResetMetric)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.handleValidateChirp)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -43,9 +62,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handleServeMetric(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverhits.Load())))
+	w.Write([]byte(fmt.Sprintf("<html><body> <h1>Welcome, Chirpy Admin</h1> <p>Chirpy has been visited %d times!</p> </body> </html>", cfg.fileserverhits.Load())))
 }
 
 func (cfg *apiConfig) handleResetMetric(w http.ResponseWriter, r *http.Request) {
@@ -53,4 +72,56 @@ func (cfg *apiConfig) handleResetMetric(w http.ResponseWriter, r *http.Request) 
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte(fmt.Sprintf("Count Reset")))
+}
+
+func (cfg *apiConfig) handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	defer r.Body.Close()
+
+	type Chirp struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var newChirp Chirp
+	err := decoder.Decode(&newChirp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{\"error\":\"Something went wrong\"}"))
+		return
+	}
+
+	var chirpCharacterCount int
+	for range newChirp.Body {
+		chirpCharacterCount++
+	}
+
+	if chirpCharacterCount > 140 {
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\":\"Chirp is too long\"}"))
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("{\"cleaned_body\":\"%s\"}", getCleanedChirpBody(newChirp.Body))))
+}
+
+func getCleanedChirpBody(chirpBody string) string {
+	var cleanedChirpBody string
+
+	bannedWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	for _, word := range strings.Fields(chirpBody) {
+		if _, ok := bannedWords[strings.ToLower(word)]; ok {
+			cleanedChirpBody = cleanedChirpBody + " ****"
+			continue
+		}
+		cleanedChirpBody = cleanedChirpBody + " " + word
+	}
+
+	return strings.TrimSpace(cleanedChirpBody)
 }
